250f9ccfda9a629aedf669a0781601df5e016946
watanabe_toshimitsu <watanabe_toshimitsu@cyberagent.co.jp>
2014-06-08 20:55:00 +0900

    add UIUnityRenderer

---
+++ Editor/UIUnityRendererInspector.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+
+[CanEditMultipleObjects]
+[CustomEditor (typeof(UIUnityRenderer), true)]
+public class UIUnityRendererInspector : UIWidgetInspector
+{
+	UIUnityRenderer mRenderer;
+	bool foldout = true;
+
+	protected override void OnEnable ()
+	{
+		base.OnEnable ();
+		mRenderer = target as UIUnityRenderer;
+	}
+
+	protected override bool ShouldDrawProperties ()
+	{
+		SerializedProperty sp = serializedObject.FindProperty ("renderQueue");
+
+		if (sp != null) {
+			EditorGUILayout.BeginHorizontal ();
+			EditorGUILayout.LabelField ("RenderQueue", sp.intValue.ToString ());
+			EditorGUILayout.EndHorizontal ();
+		}
+
+		mRenderer.allowSharedMaterial = EditorGUILayout.Toggle ("Use Shared Material", mRenderer.allowSharedMaterial);
+
+		foldout = EditorGUILayout.Foldout (foldout, "Materials");
+		if (foldout) {
+			sp = serializedObject.FindProperty ("mMats");
+			if (sp != null) {
+				for (int i = 0; i < sp.arraySize; i++) {
+					NGUIEditorTools.DrawProperty ("Material" + i, sp.GetArrayElementAtIndex (i));
+				}
+			}
+		}
+		return true;
+	}
+
+}
---
+++ UIUnityRenderer.cs
@@ -0,0 +1,153 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// NGUIでRenderer,ParticleSystemを扱うコンポーネント
+/// </summary>
+public class UIUnityRenderer : UIWidget
+{
+	// 指定されているMaterialの値を直接変更します（NGUIのレイアウトにより変更点が都度発生するので注意）
+	public bool allowSharedMaterial = false;
+
+	[HideInInspector][SerializeField] Renderer mRenderer;
+
+	[HideInInspector][SerializeField] int renderQueue = -1;
+
+	[HideInInspector][SerializeField] Material[] mMats;
+
+	public Renderer cachedRenderer {
+		get {
+			if (mRenderer == null) {
+				mRenderer = renderer;
+			}
+			return mRenderer;
+		}
+	}
+
+	/// <summary>
+	/// Material used by Renderer.
+	/// </summary>
+	public override Material material {
+		get {
+			if (ExistSharedMaterial0 () == false) {
+				Debug.LogError ("Renderer or Material is not found.");
+				return null;
+			}
+			if (allowSharedMaterial == false) {
+				if (mMats == null || mMats.Length == 0) {
+					mMats = new Material[cachedRenderer.sharedMaterials.Length];
+					for (int i = 0; i < cachedRenderer.sharedMaterials.Length; i++) {
+						mMats [i] = new Material (cachedRenderer.sharedMaterials [i]);
+						mMats [i].name = mMats [i].name + " (Copy)";
+					}
+				}
+				if (mMats != null && mMats.Length > 0) {
+					if (Application.isPlaying) {
+						if (cachedRenderer.materials != mMats) {
+							cachedRenderer.materials = mMats;
+						}
+					}
+				}
+				// NGUIには0番目で登録する
+				return mMats [0];
+			} else {
+				// SharedMaterial index0をNGUIに登録する
+				return cachedRenderer.sharedMaterials [0];
+			}
+		}
+		set {
+			throw new System.NotImplementedException (GetType () + " has no material setter");
+		}
+	}
+
+	/// <summary>
+	/// Shader used by Renderer material.
+	/// </summary>
+	public override Shader shader {
+		get {
+			if (allowSharedMaterial == false) {
+				if (mMats != null && mMats.Length > 0 && mMats [0] != null) {
+					return mMats [0].shader;
+				}
+			} else {
+				if (ExistSharedMaterial0 ()) {
+					return cachedRenderer.sharedMaterials [0].shader;
+				}
+			}
+			return null;
+		}
+		set {
+			throw new System.NotImplementedException (GetType () + " has no shader setter");
+		}
+	}
+
+	/// <summary>
+	/// SharedMaterialに一つでもマテリアルが存在するかチェック
+	/// </summary>
+	private bool ExistSharedMaterial0 ()
+	{
+		if (cachedRenderer != null &&
+		    cachedRenderer.sharedMaterials != null &&
+		    cachedRenderer.sharedMaterials.Length > 0 &&
+		    cachedRenderer.sharedMaterials [0] != null) {
+			return true;
+		}
+		return false;
+	}
+
+	protected override void OnDisable ()
+	{	
+		if (mMats != null) {
+			for (int i = 0; i < mMats.Length; i++) {
+				DestroyImmediate (mMats [i]);
+				mMats [i] = null;
+			}
+			mMats = null;
+		}
+		base.OnDisable ();
+	}
+
+	protected override void OnUpdate ()
+	{
+		base.OnUpdate ();
+		if (allowSharedMaterial == false) {
+			if (mMats != null && mMats.Length > 0 && this.drawCall != null) {
+				renderQueue = drawCall.finalRenderQueue;
+				for (int i = 0; i < mMats.Length; i++) {
+					if (mMats [i].renderQueue != renderQueue) {
+						mMats [i].renderQueue = renderQueue;
+					}
+				}
+			}
+		} else {
+			if (ExistSharedMaterial0 () && drawCall != null) {
+				renderQueue = drawCall.finalRenderQueue;
+				for (int i = 0; i < cachedRenderer.sharedMaterials.Length; i++) {
+					cachedRenderer.sharedMaterials [i].renderQueue = renderQueue;
+				}
+			}
+		}
+	}
+
+	/// <summary>
+	/// Dammy Mesh
+	/// </summary>
+	public override void OnFill (BetterList<Vector3> verts, BetterList<Vector2> uvs, BetterList<Color32> cols)
+	{
+
+		verts.Add (new Vector3 (10000f, 10000f));
+		verts.Add (new Vector3 (10000f, 10000f));
+		verts.Add (new Vector3 (10000f, 10000f));
+		verts.Add (new Vector3 (10000f, 10000f));
+
+		uvs.Add (new Vector2 (0f, 0f));
+		uvs.Add (new Vector2 (0f, 1f));
+		uvs.Add (new Vector2 (1f, 1f));
+		uvs.Add (new Vector2 (1f, 0f));
+
+		cols.Add (color);
+		cols.Add (color);
+		cols.Add (color);
+		cols.Add (color);
+	}
+}
